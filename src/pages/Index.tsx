dd414cd8913fa20f3bedb7eef8a2569fbff2065f
foxane <65512187+foxane@users.noreply.github.com>
2025-02-05 16:13:47 +0700

    Rename usePost to useFeed

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,19 @@
 import { twMerge } from "tailwind-merge";
-import usePost from "../hooks/usePost";
+import useFeed from "../hooks/useFeed";
+import { useState } from "react";
 
 export default function IndexPage() {
-  const { query, mutation } = usePost();
+  const {
+    query,
+    createMutation,
+    updateMutation,
+    likeMutation,
+    deleteMutation,
+  } = useFeed();
+  const [toUpdate, setToUpdate] = useState<Post | null>(null);
+
+  const removePTag = (text: string) =>
+    text.replace(/<p>/g, "").replace(/<\/p>/g, "\n");
 
   return (
     <div className="container mx-auto">
@@ -13,7 +24,7 @@ export default function IndexPage() {
           const text = formData.get("text") as string;
 
           if (!text) return;
-          mutation.mutate({ text });
+          createMutation.mutate({ text });
         }}
       >
         <textarea
@@ -26,7 +37,7 @@ export default function IndexPage() {
         <button
           type="submit"
           className="btn btn-primary"
-          disabled={mutation.isPending}
+          disabled={createMutation.isPending}
         >
           Post
         </button>
@@ -36,11 +47,14 @@ export default function IndexPage() {
         {query.data?.map((el) => (
           <div
             key={el.id}
-            className={twMerge("p-2 shadow-lg", el.pending && "animate-pulse")}
+            className={twMerge(
+              "p-2 shadow-lg",
+              el.isPending && "animate-pulse",
+            )}
           >
             <div className="card-title">
               <p>{el.user.name}</p>
-              {el.pending && (
+              {el.isPending && (
                 <span className="ms-auto text-sm">Posting...</span>
               )}
             </div>
@@ -53,11 +67,70 @@ export default function IndexPage() {
             </div>
 
             <div className="card-actions">
-              <button className="btn">Like {el._count.likedBy}</button>
+              <button
+                disabled={likeMutation.isPending}
+                className="btn"
+                onClick={() => likeMutation.mutate(el)}
+              >
+                Like {el.isPending ? 0 : el._count.likedBy}
+              </button>
               <button className="btn">Comment</button>
+              <button
+                className="btn"
+                onClick={() => {
+                  const modal = document.getElementById(
+                    "update-post-modal",
+                  ) as HTMLDialogElement;
+                  modal.showModal();
+                  setToUpdate(el);
+                }}
+              >
+                Update
+              </button>
+              <button
+                className="btn"
+                onClick={() => {
+                  deleteMutation.mutate(el);
+                }}
+              >
+                Delete
+              </button>
             </div>
           </div>
         ))}
+
+        <dialog className="modal" id="update-post-modal">
+          <div className="modal-box">
+            <form
+              method="dialog"
+              onSubmit={() => {
+                if (!toUpdate) return;
+                updateMutation.mutate(toUpdate);
+              }}
+            >
+              <p className="pb-5 text-lg font-semibold">Update post</p>
+
+              <textarea
+                minLength={3}
+                maxLength={300}
+                name="text"
+                className="textarea w-full"
+                value={removePTag(toUpdate?.text ?? "")}
+                onChange={(e) => {
+                  if (!toUpdate) return;
+                  setToUpdate({ ...toUpdate, text: e.target.value });
+                }}
+              />
+
+              <div className="modal-action">
+                <button className="btn">Cancel</button>
+                <button type="submit" className="btn btn-primary">
+                  Update
+                </button>
+              </div>
+            </form>
+          </div>
+        </dialog>
       </section>
     </div>
   );
